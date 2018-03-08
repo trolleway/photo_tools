27adc0bb5ac7ee236914d6736d97baadb39a0091
Artem Svetlov <trolleway@yandex.ru>
2018-03-08 21:06:46 +0300

    Update shiftn_process.py

--- photo_geocode/shiftn_process.py
+++ photo_geocode/shiftn_process.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python
 
-#Call ShiftN with dtraightforward process, then call photo_geocode on corrected photo
+
 
 import sys
 import os
 
+def get_args():
+    import argparse
+    p = argparse.ArgumentParser(description='Call ShiftN with straightforward process, then call photo_geocode on corrected photo')
+    p.add_argument('path', help='Location of one JPG file. GPS Dest Coordinates are required.')
+
+    return p.parse_args()
+
 shiftn_path = 'D:\Programs\ShiftN\ShiftN.exe'
 
-os.system('{shiftn_path} {photo} {photo_basename}_ShiftN.jpg'.format(shiftn_path = shiftn_path))
-os.system('python photo_geocode.py {photo_basename}_ShiftN.jpg'.format(shiftn_path = shiftn_path))
+if __name__ == '__main__':
+    args = get_args() 
+    photo = args.path
+    photo_basename = os.path.splitext(photo)[0]
+    
+    cmd = '{shiftn_path} {photo} {photo_basename}_ShiftN.jpg'.format(shiftn_path = shiftn_path)
+    print cmd
+    
+    cmd = 'python photo_geocode.py {photo_basename}_ShiftN.jpg'.format(shiftn_path = shiftn_path)
+    print cmd
+    
+    quit()
+    #os.system()
+    #os.system()
