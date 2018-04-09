63e8d54385652b357ef5f09329fecda5c2524f76
Artem Svetlov <trolleway@yandex.ru>
2018-04-09 14:44:11 +0300

    Create multifolder.bat

---
+++ photos2map/multifolder.bat
@@ -0,0 +1,131 @@
+SET scene=2017-11-03-01
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-03\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-03-03
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-03\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-03-04
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-03\%scene%\
+REN photos.geojson %scene%.geojson
+
+
+SET scene=2017-11-04-01
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-04\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-04-02
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-04\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-04-03
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-04\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-04-04
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-04\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-04-05
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-04\%scene%\
+REN photos.geojson %scene%.geojson
+
+
+
+SET scene=2017-11-05-01
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+
+SET scene=2017-11-05-02
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-05-03
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-05-05
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-05-06
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-05-07
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-05-08
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-05-09
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-05\%scene%\
+REN photos.geojson %scene%.geojson
+
+
+
+SET scene=2017-11-06-01
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-06\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-06-02
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-06\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-06-03
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-06\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-06-04
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-06\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-06-05
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-06\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-06-06
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-06\%scene%\
+REN photos.geojson %scene%.geojson
+
+
+SET scene=2017-11-07-01
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-07\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-07-02
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-07\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-07-03
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-07\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-07-04
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-07\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-07-05
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-07\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-07-06
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-07\%scene%\
+REN photos.geojson %scene%.geojson
+
+
+SET scene=2017-11-08-01
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-08\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-08-02
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-08\%scene%\
+REN photos.geojson %scene%.geojson
+
+SET scene=2017-11-08-03
+python photo_tools/photos2map/photos2geojson.py d:\trolleway\2017-12-25_ortophoto\source\source\2017-11-08\%scene%\
+REN photos.geojson %scene%.geojson
