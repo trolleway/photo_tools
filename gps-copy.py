f042a1d5b08103387aa3900686602d62fbfb1afe
Artem Svetlov <trolleway@yandex.ru>
2023-03-24 02:45:54 +0300

    gps-copy.py

---
+++ gps-copy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import os, subprocess
+
+directory='dirname'
+jpg_exts=('jpg')
+raw_exts=('dng','orf')
+for src_extension in jpg_exts:
+    for dst_extension in raw_exts:
+        cmd = ['exiftool', '-tagsfromfile', '%d%f'+src_extension, '-gps:all', '-ext', dst_extension, directory]
+        proc = subprocess.Popen(cmd)
+        print("the commandline is {}".format(proc.args))
