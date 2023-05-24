7cad6e8b35cd4c496bc63daa5c3910c17ba67ec4
Artem Svetlov <trolleway@yandex.ru>
2023-05-24 16:40:18 +0300

    Update gps-copy.py
    remove disused

--- gps-copy.py
+++ gps-copy.py
@@ -12,35 +12,7 @@ for src_extension in jpg_exts:
         cmd = ['/opt/exiftool/exiftool', '-tagsfromfile', 
                '%d%f'+src_extension, 
                '-overwrite_original', 
-            #"-filecreatedate<SubSecDateTimeOriginal", 
-            #"-SubSecDateTimeOriginal<datetimeoriginal",
             '-preserve',
-            #'-EXIF:OffsetTime*=+3:00',
-            #"-FileModifyDate<EXIF:DateTimeOriginal",
-            
             '-gps:all', '-ext', dst_extension, directory]
 
         subprocess.run(cmd)
-        #proc = subprocess.Popen(cmd)
-        #print("the commandline is {}".format(proc.args)) 
-
-        for root, dirs, files in os.walk(directory):
-            for name in files:
-                
-                if name.upper().endswith(dst_extension.upper()):
-
-                    cmd = ['/opt/exiftool/exiftool', '-DateTimeOriginal', '-s',
-                    '-t', '-d', '''%Y%m%d%H%M.%S''', 
-                    '-csv','-csvDelim',';' ,
-                    os.path.join(directory,name),
-                    ]
-                    
-                    photo_timestamp = subprocess.check_output(cmd).decode()
-                    photo_timestamp = photo_timestamp.splitlines()[-1]
-                    photo_timestamp = photo_timestamp.rpartition(';')[-1]
-                    
-                    #print(photo_timestamp)
-                    #cmd = ['TZ=UTC ','touch','-t',photo_timestamp,os.path.join(directory,name)]
-                    #subprocess.run(cmd)
-
-    
