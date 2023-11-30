288fb1f50c588c1643fbccaffd312b9d871be120
Artem Svetlov <trolleway@yandex.ru>
2023-11-30 23:08:11 +0300

    line endings

--- gps-copy.py
+++ gps-copy.py
@@ -9,6 +9,7 @@ jpg_exts.append('.jpg')
 raw_exts=('dng','orf','nef')
 for src_extension in jpg_exts:
     for dst_extension in raw_exts:
+        print(f'{src_extension} -> {dst_extension}')
         cmd = ['/opt/exiftool/exiftool', '-tagsfromfile', 
                '%d%f'+src_extension, 
                '-overwrite_original', 
