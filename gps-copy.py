ceb2a4e04487ccb8eaa43b4cbd3de34a8c647bda
Artem Svetlov <trolleway@yandex.ru>
2025-03-04 23:53:56 +0300

    gps-copy add tif

--- gps-copy.py
+++ gps-copy.py
@@ -6,7 +6,7 @@ import os, subprocess
 directory='photos'
 jpg_exts=list()
 jpg_exts.append('.jpg')
-raw_exts=('dng','orf','nef','mpo')
+raw_exts=('dng','orf','nef','mpo','tif')
 for src_extension in jpg_exts:
     for dst_extension in raw_exts:
         print(f'copy tags from {src_extension} to {dst_extension}')
